a65e8910476cc637098be40531b09495680d0d68
syed-shajahan <81956288+syed-shajahan@users.noreply.github.com>
2023-10-02 21:33:08 +0530

    initial commit two

--- src/app/Components/GridSection/index.tsx
+++ src/app/Components/GridSection/index.tsx
@@ -6,35 +6,39 @@ import Grid from "@mui/material/Unstable_Grid2";
 import { GridWrapBox, GridWrapImg } from "./styles";
 import { CustomContainer } from "../Banner/styles";
 import About from "../About";
-
 const GridSec = () => {
   return (
     <>
-    <CustomContainer>
-
-    <Box sx={{ flexGrow: 1, margin:"20px 0px" }}>
-      <Grid container spacing={2}>
-        <Grid xs={6}>
-          <GridWrapBox >
-            <GridWrapImg src="/assets/images/g-1.jpg" layout="fill" alt="fashion"/>
-          </GridWrapBox>
-        </Grid>
-        <Grid xs={6}>
-
-          <Box>
-            <About/>
-          </Box>
-
-        </Grid>
-        <Grid xs={6}></Grid>
-        <Grid xs={6}>
-        <GridWrapBox sx={{paddingTop: "89%!important"}}>
-            <GridWrapImg src="/assets/images/g-2.jpg" layout="fill" alt="fashion"/>
-        </GridWrapBox>
-        </Grid>
-      </Grid>
-    </Box>
-    </CustomContainer>
+      <CustomContainer>
+        <Box sx={{ flexGrow: 1, margin: "20px 0px" }}>
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+              <GridWrapBox>
+                <GridWrapImg
+                  src="/assets/images/g-1.jpg"
+                  layout="fill"
+                  alt="fashion"
+                />
+              </GridWrapBox>
+            </Grid>
+            <Grid xs={6}>
+              <Box>
+                <About />
+              </Box>
+            </Grid>
+            <Grid xs={6}></Grid>
+            <Grid xs={6}>
+              <GridWrapBox sx={{ paddingTop: "89%!important" }}>
+                <GridWrapImg
+                  src="/assets/images/g-2.jpg"
+                  layout="fill"
+                  alt="fashion"
+                />
+              </GridWrapBox>
+            </Grid>
+          </Grid>
+        </Box>
+      </CustomContainer>
     </>
   );
 };
