7624a311daa9c246200412924d3861de682813ce
syed-shajahan <shajahn7shaju7@gmail.com>
2023-10-19 19:56:31 +0530

    contact card section added

--- src/app/Components/Banner/styles.ts
+++ src/app/Components/Banner/styles.ts
@@ -1,34 +1,34 @@
-"use client"
-import React from 'react';
-import {Box} from '@mui/material'; 
-import Image from 'next/image';
-import styled from '@mui/system/styled';
+"use client";
+import React from "react";
+import {Box} from "@mui/material";
+import Image from "next/image";
+import styled from "@mui/system/styled";
 
-export const CustomContainer = styled(Box)(({ theme }) => ({
+export const CustomContainer = styled(Box)(({theme}) => ({
     display: "block",
     width: "100%",
     maxWidth: "1840px",
     margin: "auto",
     padding: "0px 10px"
-  }));
+}));
 
-export const BannerCard = styled(Box)(({ theme }) => ({
+export const BannerCard = styled(Box)(({theme}) => ({
     display: "block",
     width: "100%",
-    paddingTop: "47%",
-    position:"relative",
-    overflow:"hidden",
-    borderRadius:"8px",
-    marginBottom:"22px"
-  }));
+    paddingTop: "50%",
+    position: "relative",
+    overflow: "hidden",
+    borderRadius: "8px",
+    marginBottom: "22px"
+}));
 
-  export const BannerImgContent = styled(Image)(({ theme }) => ({
-    position:"absolute",
-    display:"block",
-    top:"0px",
-    left:"0px",
-    width:"100% !important",
-    height:"100% !important",
-    ObjectFit:"cover",
-    objectPosition:"center"
-  }));
+export const BannerImgContent = styled(Image)(({theme}) => ({
+    position: "absolute",
+    display: "block",
+    top: "0px",
+    left: "0px",
+    width: "100% !important",
+    height: "100% !important",
+    ObjectFit: "cover",
+    objectPosition: "center"
+}));
---
+++ src/app/Components/contactcard/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import Grid from "@mui/material/Grid/Grid";
+import Box from "@mui/material/Box/Box";
+
+const ContactCard = () => {
+    return (<Box>
+        <Grid container
+            spacing={2}>
+            <Grid item  xs={4}>  
+            
+            </Grid>
+            <Grid item
+                xs={4}></Grid>
+            <Grid item
+                xs={4}></Grid>
+            <Grid item
+                xs={4}></Grid>
+        </Grid>
+    </Box>)
+}
+
+export default ContactCard;
