abf702775f7a69a7f195da0736f7206819747578
syed-shajahan <81956288+syed-shajahan@users.noreply.github.com>
2023-11-12 21:38:56 +0530

    CSS custmoization

--- src/app/Components/latest-news/index.tsx
+++ src/app/Components/latest-news/index.tsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { CustomContainer } from '../Banner/styles';
-import Box from '@mui/material/Box';
+import React from "react";
+import { CustomContainer } from "../Banner/styles";
+import Box from "@mui/material/Box";
 
-import Typography from '@mui/material/Typography';
-import { GitLink } from '../Contact/styles';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { Contact } from '../Contact/constant';
-import { IcnWrap } from '../List-Cards/styles';
-import Link from 'next/link';
+import Typography from "@mui/material/Typography";
+import { GitLink } from "../Contact/styles";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Contact } from "../Contact/constant";
+import { IcnWrap } from "../List-Cards/styles";
+import Link from "next/link";
+import { LinkTag } from "./styles";
 const Latestnews = () => {
   return (
     <Box>
       <CustomContainer>
         <Box
           className="titelWarp"
-          display={'flex'}
-          alignItems={'center'}
-          justifyContent={'space-between'}
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
         >
           <Typography variant="h3" className="titleTag">
             Latest News
           </Typography>
 
-          <Link href="#"  className='links_target'>
+          <LinkTag href="#" className="links_target">
             <IcnWrap className="sb_icn sb_icn_sm">
               <ArrowForwardIcon />
             </IcnWrap>
             See All
-          </Link>
+          </LinkTag>
         </Box>
       </CustomContainer>
     </Box>
--- src/app/Components/latest-news/styles.ts
+++ src/app/Components/latest-news/styles.ts
@@ -1,9 +1,17 @@
+"use client"
 import styled from '@mui/system/styled';
 
 import Link from 'next/link';
 
-
 export const LinkTag = styled(Link)(()=>({
-    display:"flex",
-    alignItems:"center",
-}))
+    display:'flex',
+    alignItems:'center',
+    color: "rgba(255, 255, 255, 0.70)",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    fontStyle: "normal",
+    fontWeight: "500",
+    lineHeight: "normal",
+    letterSpacing: "0.96px",
+    textDecoration:"none"
+}));
